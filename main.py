3597a0f956e9a2254f1d3e71136d3234e4ab5ab8
ilem-ilem <therealteckyguys@gmail.com>
2026-05-25 22:22:22 +0100

    added the auth pages and route

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="templates")
 
-
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse(request, "index.html")
+
+
+@app.get("/auth/login")
+def login(request: Request):
+    return templates.TemplateResponse(request, "auth/login.html")
+
+
+@app.get("/auth/register")
+def register(request: Request):
+    return templates.TemplateResponse(request, "auth/register.html")
